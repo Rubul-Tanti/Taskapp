4b3d5ec8111773605701a54255360abbd2e178ec
Rubul-Tanti <rubultanti404@gmail.com>
2025-11-20 22:08:50 +0530

    updating cors

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,7 @@ const corsconfig=()=>{
 
    return cors({
         origin:(orgin,callback)=>{
-            const allowedOrigins=['http://localhost:3000','']
+            const allowedOrigins=['http://localhost:3000','https://taskapp-15qic29cq-rubuls-projects.vercel.app']
             if(allowedOrigins.indexOf(orgin)!==-1||!orgin){
                 callback(null,true)
             }else{new Eroor('Not Allowed By CORS')
